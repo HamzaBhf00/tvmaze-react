8d930647300748af86ab02579ee64d6247ae9ad6
HamzaBhf00 <alumno.1248865@ies-azarquiel.es>
2023-06-14 09:12:56 +0200

    Terminar Footer y About

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
+    <footer className="bg-dark text-light">
+      <div className="container py-5">
+        <div className="row">
+          <div className="col-md-6">
+            <h4>Información de contacto</h4>
+            <p>123 Calle Principal, Ciudad</p>
+            <p>Teléfono: (123) 456-7890</p>
+            <p>Email: info@example.com</p>
+          </div>
+          <div className="col-md-6">
+            <h4>Síguenos en las redes sociales</h4>
+            <ul className="list-inline">
+              <li className="list-inline-item">
+                <a href="#" className="social-link"><i className="fab fa-facebook-f"></i></a>
+              </li>
+              <li className="list-inline-item">
+                <a href="#" className="social-link"><i className="fab fa-twitter"></i></a>
+              </li>
+              <li className="list-inline-item">
+                <a href="#" className="social-link"><i className="fab fa-instagram"></i></a>
+              </li>
+              <li className="list-inline-item">
+                <a href="#" className="social-link"><i className="fab fa-linkedin"></i></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/components/Routers/About.js
+++ src/components/Routers/About.js
@@ -1,9 +1,94 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>About</div>
-  )
+    <div>
+      <div className="bg-light py-5">
+        <div className="container py-5">
+          <div className="row mb-4">
+            <div className="col-lg-5">
+              <h2 className="display-4 font-weight-light">Sobre Nosotros</h2>
+              <p className="font-italic text-muted">Somos una organización que organiza películas y series.</p>
+            </div>
+          </div>
+
+          <div className="row text-center">
+            <div className="col-xl-3 col-sm-6 mb-5">
+              <div className="bg-white rounded shadow-sm py-5 px-4">
+                <img src="https://bootstrapious.com/i/snippets/sn-about/avatar-1.png" alt="" width="100" className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
+                <h5 className="mb-0">Hamza Boulahfa</h5><span className="small text-uppercase text-muted">CEO - Fundador</span>
+                <ul className="social mb-0 list-inline mt-3">
+                  <li className="list-inline-item">
+                    <a href="#" className="social-link"><i className="fab fa-facebook"></i></a>
+                  </li>                  <li className="list-inline-item">
+                    <a href="#" className="social-link"><i className="fab fa-instagram"></i></a>
+                  </li>                  <li className="list-inline-item">
+                    <a href="#" className="social-link"><i className="fab fa-youtube"></i></a>
+                  </li>                  <li className="list-inline-item">
+                    <a href="#" className="social-link"><i className="fab fa-linkedin"></i></a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="col-xl-3 col-sm-6 mb-5">
+              <div className="bg-white rounded shadow-sm py-5 px-4">
+                <img src="https://bootstrapious.com/i/snippets/sn-about/avatar-2.png" alt="" width="100" className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
+                <h5 className="mb-0">Samuel Hardy</h5><span className="small text-uppercase text-muted">Ayudante</span>
+                <ul className="social mb-0 list-inline mt-3">
+                  <li className="list-inline-item">
+                    <a href="#" className="social-link"><i className="fab fa-facebook"></i></a>
+                  </li>                  <li className="list-inline-item">
+                    <a href="#" className="social-link"><i className="fab fa-instagram"></i></a>
+                  </li>                  <li className="list-inline-item">
+                    <a href="#" className="social-link"><i className="fab fa-youtube"></i></a>
+                  </li>                  <li className="list-inline-item">
+                    <a href="#" className="social-link"><i className="fab fa-linkedin"></i></a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="col-xl-3 col-sm-6 mb-5">
+              <div className="bg-white rounded shadow-sm py-5 px-4">
+                <img src="https://bootstrapious.com/i/snippets/sn-about/avatar-3.png" alt="" width="100" className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
+                <h5 className="mb-0">Tom Sunderland</h5><span className="small text-uppercase text-muted">Ayudante</span>
+                <ul className="social mb-0 list-inline mt-3">
+                  <li className="list-inline-item">
+                    <a href="#" className="social-link"><i className="fab fa-facebook"></i></a>
+                  </li>                  <li className="list-inline-item">
+                    <a href="#" className="social-link"><i className="fab fa-instagram"></i></a>
+                  </li>                  <li className="list-inline-item">
+                    <a href="#" className="social-link"><i className="fab fa-youtube"></i></a>
+                  </li>                  <li className="list-inline-item">
+                    <a href="#" className="social-link"><i className="fab fa-linkedin"></i></a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="col-xl-3 col-sm-6 mb-5">
+              <div className="bg-white rounded shadow-sm py-5 px-4">
+                <img src="https://bootstrapious.com/i/snippets/sn-about/avatar-4.png" alt="" width="100" className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
+                <h5 className="mb-0">Sara Weles</h5><span className="small text-uppercase text-muted">Ayudante</span>
+                <ul className="social mb-0 list-inline mt-3">
+                  <li className="list-inline-item">
+                    <a href="#" className="social-link"><i className="fab fa-facebook"></i></a>
+                  </li>                  <li className="list-inline-item">
+                    <a href="#" className="social-link"><i className="fab fa-instagram"></i></a>
+                  </li>                  <li className="list-inline-item">
+                    <a href="#" className="social-link"><i className="fab fa-youtube"></i></a>
+                  </li>                  <li className="list-inline-item">
+                    <a href="#" className="social-link"><i className="fab fa-linkedin"></i></a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default About
+export default About;
